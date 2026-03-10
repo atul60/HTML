65ee444c5a3479d576d25f8951b462f476fba625
Atul Singh <satul388@gmail.com>
2026-03-11 00:02:28 +0530

    grid css explained

--- javascript.js
+++ javascript.js
@@ -1,14 +1,23 @@
-function loadPage(pagePath) {
+function loadPage(pagePath, stylePath) {
         fetch(pagePath)
           .then(response => response.text())
           .then(html => {
             // Extract body content from the loaded HTML
             const parser = new DOMParser();
             const doc = parser.parseFromString(html, 'text/html');
+            console.log('Loaded HTML:', doc);
             const bodyContent = doc.body.innerHTML;
             
             // Inject into container
             document.getElementById('content-container').innerHTML = bodyContent;
+
+            // Load the corresponding stylesheet
+            if (stylePath) {
+              const link = document.createElement('link');
+              link.rel = 'stylesheet';
+              link.href = stylePath;
+              document.head.appendChild(link);
+            }
           })
           .catch(error => {
             document.getElementById('content-container').innerHTML = 
